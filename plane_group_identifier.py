cb8685192e2a3a0e746d635100915948caa4822d
Thomas Branfield <tbranfield@gmail.com>
2023-07-15 18:03:11 +0100

    Wrote get_highest_order_rotation() function

--- plane_group_identifier.py
+++ plane_group_identifier.py
@@ -6,17 +6,35 @@ Usage:
     ./identifying_plane_groups.py
 """
 
+import sys
 
-def get_highest_order_rotation():
+
+QUIT_STRINGS = ["q", "quit", "exit"]
+
+
+def quit_program(self):
+    print("Program aborted.")
+    sys.exit()
+
+
+def get_highest_order_rotation(self):
     """
     Prompts the user for the highest order of rotation present
     in the pattern.
     """
-    pass
+    prompt_message = "What is the highest order of rotation of the pattern?"
+    while True:
+        user_input = input(prompt_message)
+        if user_input in QUIT_STRINGS:
+            quit_program()
+        if is_valid_order_rotation(input):
+            return user_input
+        else:
+            print("Invalid input. Please enter None, 2, 3, 4, or 6.")
+        
 
-
-def is_valid_order_rotation(n):
-    """
-    Tests whether a given input is a valid order of rotation.
-    """
-    pass
+    def is_valid_order_rotation(self):
+        """
+        Tests whether a given input is a valid order of rotation.
+        """
+        pass
