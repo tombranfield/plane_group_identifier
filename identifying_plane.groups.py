ffbe8112e317e43a53382ee5b3753902e420830f
Thomas Branfield <tbranfield@gmail.com>
2023-07-15 16:18:31 +0100

    Added docstring to identifying_plane_groups.py

--- identifying_plane.groups.py
+++ identifying_plane.groups.py
@@ -1,1 +1,22 @@
-# Identifying plane groups
+"""
+Identifies a plane group of a two-dimensional pattern given
+user-supplied information.
+
+Usage:
+    ./identifying_plane_groups.py
+"""
+
+
+def get_highest_order_rotation():
+    """
+    Prompts the user for the highest order of rotation present
+    in the pattern.
+    """
+    pass
+
+
+def is_valid_order_rotation(n):
+    """
+    Tests whether a given input is a valid order of rotation.
+    """
+    pass
