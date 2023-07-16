d9d908a06ebd44f74b9a75e0bfdfb54e39a08087
Thomas Branfield <tbranfield@gmail.com>
2023-07-16 15:00:03 +0100

    Added todo notes on test file

--- test_plane_group_identifier.py
+++ test_plane_group_identifier.py
@@ -6,14 +6,14 @@ from plane_group_identifier import *
 import pytest
 
 
-@pytest.mark.parametrize("in_num", [1, 2, 3, 4, 6])
-def test_is_valid_order_rotation_true(in_num):
-    assert is_valid_order_rotation(in_num) == True
+@pytest.mark.parametrize("correct_input", [1, 2, 3, 4, 6])
+def test_is_valid_order_rotation_true(correct_input):
+    assert is_valid_order_rotation(correct_input) == True
 
 
-@pytest.mark.parametrize("in_num", [0, 5, 7, "string", None])
-def test_is_valid_order_rotation_false(in_num):
-    assert is_valid_order_rotation(in_num) == False
+@pytest.mark.parametrize("invalid_input", [0, 5, 7, "string", None])
+def test_is_valid_order_rotation_false(invalid_input):
+    assert is_valid_order_rotation(invalid_input) == False
 
 
 
@@ -27,6 +27,7 @@ def test_is_valid_order_rotation_false(in_num):
 # In fact, I'll setup a sandbox for learning it
 # Copy lots of examples etc
 # So leave this coding for a bit...
+# Need to learn about fixtures, mocking, and monkeypatch
 def test_get_highest_order_rotation():
     assert (1, 2, 3) == (1, 2, 3)
 
