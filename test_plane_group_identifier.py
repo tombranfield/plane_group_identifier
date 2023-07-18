38efdf6a3cec2a7b383612f592e391c3c97101d9
Thomas Branfield <tbranfield@gmail.com>
2023-07-18 20:58:48 +0100

    Added the constants VALID_ORDERS_OF_ROTATION, YES_PHRASES, no NO_PHRASES to test module

--- test_plane_group_identifier.py
+++ test_plane_group_identifier.py
@@ -6,9 +6,14 @@ from plane_group_identifier import *
 import pytest
 
 
-# TODO put valid orders of rotation into a list in a fixture
+# Note these are copied as-is to the tests (not copied), so do not
+# have code that mutates them as further tests will be affected.
+VALID_ORDERS_OF_ROTATION = [1, 2, 3, 4, 6]
+YES_PHRASES = ["y", "yes", "Y", "YES"]
+NO_PHRASES = ["n", "no", "N", "NO"]
 
-@pytest.mark.parametrize("correct_input", [1, 2, 3, 4, 6])
+
+@pytest.mark.parametrize("correct_input", VALID_ORDERS_OF_ROTATION)
 def test_is_valid_order_rotation_true(correct_input):
     assert is_valid_order_rotation(correct_input) == True
 
@@ -18,21 +23,21 @@ def test_is_valid_order_rotation_false(invalid_input):
     assert is_valid_order_rotation(invalid_input) == False
 
 
-@pytest.mark.parametrize("valid_order", [1, 2, 3, 4, 6])
+@pytest.mark.parametrize("valid_order", VALID_ORDERS_OF_ROTATION)
 def test_get_highest_order_rotation(monkeypatch, valid_order):
     monkeypatch.setattr("builtins.input", lambda _: valid_order)
     user_response = get_highest_order_rotation()
     assert user_response == valid_order
 
 
-@pytest.mark.parametrize("yes_input", ["y", "yes", "Y", "YES"])
+@pytest.mark.parametrize("yes_input", YES_PHRASES)
 def test_yes_or_no_question_true(monkeypatch, yes_input):
     monkeypatch.setattr("builtins.input", lambda _: yes_input)
     user_response = yes_or_no_question("Hi?")
     assert user_response == True
 
 
-@pytest.mark.parametrize("no_input", ["n", "no", "N", "NO"])
+@pytest.mark.parametrize("no_input", NO_PHRASES)
 def test_yes_or_no_question_false(monkeypatch, no_input):
     monkeypatch.setattr("builtins.input", lambda _: no_input)
     user_response = yes_or_no_question("Hi?")
