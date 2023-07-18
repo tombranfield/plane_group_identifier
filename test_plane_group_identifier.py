7d83a172bbd93918b676ef7887f04d1ba6030c47
Thomas Branfield <tbranfield@gmail.com>
2023-07-18 20:22:08 +0100

    Parametized test_get_correct_order_rotation() with all valid inputs

--- test_plane_group_identifier.py
+++ test_plane_group_identifier.py
@@ -6,13 +6,6 @@ from plane_group_identifier import *
 import pytest
 
 
-# This will hold a dictionary with key-value pairs corresponding
-# to the input and the output plane groups
-@pytest.fixture
-def correct_results():
-    return {}
-
-
 @pytest.mark.parametrize("correct_input", [1, 2, 3, 4, 6])
 def test_is_valid_order_rotation_true(correct_input):
     assert is_valid_order_rotation(correct_input) == True
@@ -23,11 +16,11 @@ def test_is_valid_order_rotation_false(invalid_input):
     assert is_valid_order_rotation(invalid_input) == False
 
 
-# Show user inputting [1, 2, 3, 4, 6] outputs that integer
-def test_get_highest_order_rotation(monkeypatch):
-    monkeypatch.setattr("builtins.input", lambda _: 1)
+@pytest.mark.parametrize("valid_order", [1, 2, 3, 4, 6, 7])
+def test_get_highest_order_rotation(monkeypatch, valid_order):
+    monkeypatch.setattr("builtins.input", lambda _: valid_order)
     user_response = get_highest_order_rotation()
-    assert user_response == 1
+    assert user_response == valid_order
 
 
 
